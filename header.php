c7d9e6efee81871120a45e1dbd88f903333e3c36
Jason Zhai <vines15@gmail.com>
2012-11-07 17:45:30 -0800

    mixed changes
    added favicon;
    changed logo color;
    top left buttons color changes;

--- header.php
+++ header.php
@@ -36,7 +36,7 @@ echo $image[0]; ?>" />
 	<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
-<!--<link rel="Shortcut Icon" type="image/ico" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" />-->
+    <link rel="Shortcut Icon" type="image/ico" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" />
 	<script type="text/javascript">
 		document.documentElement.className = 'js';
 	</script>
