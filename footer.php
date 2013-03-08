37a4651f81b1925fd51d5727d4b1503713931c9a
Jason Zhai <vines15@gmail.com>
2013-03-07 17:39:43 -0800

    Live Chat function

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 			</div> 	<!-- end #main-area -->
-		
 		<footer id="main_footer" class="clearfix">
 			<p id="copyright"><!--<span style="font-size:22px;">联系我们</span><br />长城会<br /><a href="mailto: info@gmic-sv.com" title="Email Us" target="_blank">info@gmic-sv.com</a><br /><a href="http://www.facebook.com/theGMIC" target="_blank">www.facebook.com/theGMIC</a><br /><a href="http://www.twitter.com/theGMIC" target="_blank">Twitter: @theGMIC</a><br />565 Clyde Ave, Suite 600, Mountain View, CA, 94043<br />-->Copyright © Global Mobile Internet Conference  - All rights reserved.<br />Organized by <a target="_blank" href="http://www.gwc.net">GWC</a></p>
 		</footer> <!-- end #main_footer -->
@@ -9,6 +8,7 @@
 </div>
 
 	<?php wp_footer(); ?>
+
 <script type="text/javascript" language="javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.carouFredSel-5.6.1-packed.js"></script>
 <script type="text/javascript" language="javascript">
 			$(function() {
@@ -128,5 +128,53 @@
 <div id="backtotop">
    <a href="#">&uarr; TOP</a>
 </div>
+<!-- BEGIN OLARK IMAGE TAB-->
+<style>
+    div#olark_tab{
+        position: fixed;
+        left: 0;
+        bottom:79px;
+        z-index:5000;
+    }
+    #olark_tab a {
+        display:block;
+        /*Edit these to change the look of your tab*/
+        border: 0px solid white;
+        border-left-style: none;
+        border-bottom-style: none;
+        margin-top:0px;
+    }
+    #olark_tab a:hover{
+        border-color: orange;
+    }
+</style>
+
+<div id="olark_tab">
+    <a href="javascript:void(0);" onclick="olark('api.box.expand')">
+        <img src="http://static.olark.com/images/livehelp-tab-icon.png" />
+    </a>
+</div>
+<!-- END OLARK TAB-->
+		
+<!-- begin olark code -->
+<script data-cfasync="false" type='text/javascript'>/*<![CDATA[*/window.olark||(function(c){var f=window,d=document,l=f.location.protocol=="https:"?"https:":"http:",z=c.name,r="load";var nt=function(){
+f[z]=function(){
+(a.s=a.s||[]).push(arguments)};var a=f[z]._={
+},q=c.methods.length;while(q--){(function(n){f[z][n]=function(){
+f[z]("call",n,arguments)}})(c.methods[q])}a.l=c.loader;a.i=nt;a.p={
+0:+new Date};a.P=function(u){
+a.p[u]=new Date-a.p[0]};function s(){
+a.P(r);f[z](r)}f.addEventListener?f.addEventListener(r,s,false):f.attachEvent("on"+r,s);var ld=function(){function p(hd){
+hd="head";return["<",hd,"></",hd,"><",i,' onl' + 'oad="var d=',g,";d.getElementsByTagName('head')[0].",j,"(d.",h,"('script')).",k,"='",l,"//",a.l,"'",'"',"></",i,">"].join("")}var i="body",m=d[i];if(!m){
+return setTimeout(ld,100)}a.P(1);var j="appendChild",h="createElement",k="src",n=d[h]("div"),v=n[j](d[h](z)),b=d[h]("iframe"),g="document",e="domain",o;n.style.display="none";m.insertBefore(n,m.firstChild).id=z;b.frameBorder="0";b.id=z+"-loader";if(/MSIE[ ]+6/.test(navigator.userAgent)){
+b.src="javascript:false"}b.allowTransparency="true";v[j](b);try{
+b.contentWindow[g].open()}catch(w){
+c[e]=d[e];o="javascript:var d="+g+".open();d.domain='"+d.domain+"';";b[k]=o+"void(0);"}try{
+var t=b.contentWindow[g];t.write(p());t.close()}catch(x){
+b[k]=o+'d.write("'+p().replace(/"/g,String.fromCharCode(92)+'"')+'");d.close();'}a.P(2)};ld()};nt()})({
+loader: "static.olark.com/jsclient/loader0.js",name:"olark",methods:["configure","extend","declare","identify"]});
+/* custom configuration goes here (www.olark.com/documentation) */
+olark.identify('1667-530-10-6415');/*]]>*/</script><noscript><a href="https://www.olark.com/site/1667-530-10-6415/contact" title="Contact us" target="_blank">Questions? Feedback?</a> powered by <a href="http://www.olark.com?welcome" title="Olark live chat software">Olark live chat software</a></noscript>
+<!-- end olark code -->
 </body>
 </html>
