8a213c686fcaa4b64e16994578e3ca899c1b70f2
Darren <darren@greatwallclub.com>
2013-03-28 15:55:09 +0800

    remove fb like scrpit from header
    less error connection

--- header.php
+++ header.php
@@ -37,16 +37,6 @@ echo $image[0]; ?>" />
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
     <link rel="Shortcut Icon" type="image/ico" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" />
-	<script type="text/javascript">
-		document.documentElement.className = 'js';
-	</script>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
